60a69517e50f60e647da978776f299cc0ea374e8
marymanzolillo <131816259+marymanzolillo@users.noreply.github.com>
2023-04-25 13:39:36 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+$(document).ready(function() {
+  // Initialize the FullCalendar
+  $('#calendar').fullCalendar({
+    events: [
+      // Add your event data here (you can fetch this data from your back-end later)
+      {
+        title: 'Event 1',
+        start: '2023-05-01',
+        end: '2023-05-02',
+        url: 'https://example.com/event1'
+      },
+      {
+        title: 'Event 2',
+        start: '2023-05-15',
+        end: '2023-05-16',
+        url: 'https://example.com/event2'
+      }
+    ],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    defaultView: 'month',
+    eventClick: function(event) {
+      if (event.url) {
+        window.open(event.url, '_blank');
+        return false;
+      }
+    }
+  });
+});
 
