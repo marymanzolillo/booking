93d980bdbe1f948f8720286a37d0dcef6b411f62
marymanzolillo <131816259+marymanzolillo@users.noreply.github.com>
2023-04-25 18:42:03 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -25,3 +25,41 @@ auth.onAuthStateChanged(user => {
     console.log('User signed out');
   }
 });
+
+// Get the registration form and handle its submit event
+const registerForm = document.getElementById('register-form');
+if (registerForm) {
+  registerForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = registerForm['register-email'].value;
+    const password = registerForm['register-password'].value;
+
+    try {
+      await register(email, password);
+      alert('Registration successful!');
+      // Redirect to another page or update the UI as needed
+    } catch (error) {
+      alert('Registration failed: ' + error.message);
+    }
+  });
+}
+
+// Get the sign-in form and handle its submit event
+const signInForm = document.getElementById('sign-in-form');
+if (signInForm) {
+  signInForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = signInForm['sign-in-email'].value;
+    const password = signInForm['sign-in-password'].value;
+
+    try {
+      await signIn(email, password);
+      alert('Sign in successful!');
+      // Redirect to another page or update the UI as needed
+    } catch (error) {
+      alert('Sign in failed: ' + error.message);
+    }
+  });
+}
